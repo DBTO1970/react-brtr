bfffb04d7e2965db37a1a897c8a97db8e5fc7b6b
dbto1970 <donbartojunior@gmail.com>
2021-07-31 14:45:57 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar dark color ='primary'>
+            <div className='container'>
+              <NavbarBrand href='/'> | BRTR | </NavbarBrand>
+            </div>
+          </Navbar>
+          
+        </div>
+      </BrowserRouter>
+    );
+  }
+  
 }
 
 export default App;
