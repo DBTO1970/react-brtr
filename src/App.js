76b84bfdd8250875067dc5e80a2c3e2a946b8199
dbto1970 <donbartojunior@gmail.com>
2021-08-16 09:57:24 -0400

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import Main from './components/MainComponent';
-import { BrowserRouter } from 'react-router-dom';
+import { Navbar, NavbarBrand  } from 'reactstrap';
 import './App.css';
-
-
 
 class App extends Component {
   render() {
     return (
-     
-        <BrowserRouter>
           <div className="App">
-            <Main />
+            <Navbar dark color="primary">
+              <div className="container">
+                <NavbarBrand href="/">|BRTR|</NavbarBrand>
+              </div>
+            </Navbar>
           </div>
-        </BrowserRouter>
+        
       
     );
   }
